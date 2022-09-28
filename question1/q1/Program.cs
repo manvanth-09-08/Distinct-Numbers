e1f7d8fa8246877a8821d67183ec57344fbfa2c5
Manvanth B S <110272624+manvanth-09-08@users.noreply.github.com>
2022-09-29 01:00:50 +0530

    Add files via upload

---
+++ question1/q1/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace q1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            HashSet<double> hash = new HashSet<double>();
+            for (int i = 2; i <= 100; i++)
+            {
+                for (int j = 2; j <= 100; j++)
+                {
+                    double temp = Math.Pow(i, j);
+                    hash.Add(temp);
+                }
+            }
+
+            Console.WriteLine(hash.Count);
+            Console.ReadKey();
+        }
+    }
+}
